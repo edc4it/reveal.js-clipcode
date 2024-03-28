a3ca54068be4cae5b8dde308ef00fda80d9abf5b
Raphael Parree <rparree@edc4it.com>
2024-03-28 10:35:01 +0100

    chore: restructured prep for npm

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
-const path = require('path')
-const {defineConfig} = require('vite')
+const path = require('path');
+const { defineConfig } = require('vite');
 import dts from 'vite-plugin-dts';
-import removeConsole from "vite-plugin-remove-console";
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import removeConsole from 'vite-plugin-remove-console';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 module.exports = defineConfig({
-    build: {
-        lib: {
-            entry: path.resolve(__dirname, 'lib/plugin.ts'),
-            name: 'ClipCode',
-            fileName: (format) => `ClipCode${format}.js`
-        }
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/plugin.ts'),
+      name: 'ClipCode',
+      fileName: (format) => `clipcode.${format}.js`,
     },
-    plugins: [
-        dts(),
-        removeConsole(),
-        cssInjectedByJsPlugin(), // bundle CSS into js file
-    ]
+  },
+  plugins: [
+    dts(),
+    removeConsole(),
+    cssInjectedByJsPlugin(), // bundle CSS into js file
+  ],
 });
