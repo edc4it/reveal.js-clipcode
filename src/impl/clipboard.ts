8868a756e443343b579e8a61309e508e1022281f
Raphael Parree <rparree@edc4it.com>
2025-02-04 09:47:29 +0100

    feat: removed dependency on clipboard node library

--- src/impl/clipboard.ts
+++ src/impl/clipboard.ts
@@ -1,34 +1,29 @@
-import ClipboardJS from 'clipboard';
-import { Options } from '../options.ts';
+import {Options} from '../options.ts';
 
 export function initClipboardActions(btnSelector: string, options: Options) {
-  const buttons = document.querySelectorAll<HTMLButtonElement>(btnSelector);
+    const buttons = document.querySelectorAll<HTMLButtonElement>(btnSelector);
 
-  buttons.forEach((e) =>
-    e.addEventListener('click', () => {
-      e.disabled = true;
-    }),
-  );
+    buttons.forEach((e) =>
+        e.addEventListener('click', () => {
+            e.setAttribute('disabled',"true");
 
-  const clipboard = new ClipboardJS(buttons, {
-    text: function(trigger) {
-      const code = trigger?.nextElementSibling?.querySelector('code');
-      const trimmed = code?.innerText.trim();
-      // empty string denotes "failure"
-      return trimmed ?? '';
-    },
-  });
-  clipboard.on('success', (e) => {
-    e.clearSelection();
-    const trigger = e.trigger;
-    trigger.classList.add('success');
-    setTimeout(() => {
-      trigger.classList.remove('success');
-      trigger.removeAttribute('disabled');
-    }, options.timeout);
-  });
-  clipboard.on('error', (e) => {
-    e.trigger.removeAttribute('disabled');
-    console.error('error copying to clipboard', e.action);
-  });
+            const code = e.nextElementSibling?.querySelector('code');
+            const trimmed = code?.innerText.trim() // empty string denotes "failure"
+            if (trimmed) {
+                navigator.clipboard.writeText(trimmed).then(() => {
+
+                    e.classList.add('success');
+                    setTimeout(() => {
+                        e.classList.remove('success');
+                        e.removeAttribute('disabled');
+                    }, options.timeout);
+
+                }).catch((error) => {
+                    e.removeAttribute('disabled');
+                    console.error('error copying to clipboard', error.action);
+                })
+            }
+
+        }),
+    );
 }
