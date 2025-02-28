67d19b95d11e0ba10c8e17af2dfab5c32058caf4
Raphael Parree <rparree@edc4it.com>
2025-02-28 15:37:58 +0100

    feat: trim $ from shell code blocks

--- src/impl/clipboard.ts
+++ src/impl/clipboard.ts
@@ -1,29 +1,46 @@
-import {Options} from '../options.ts';
+import { Options } from '../options.ts';
 
 export function initClipboardActions(btnSelector: string, options: Options) {
-    const buttons = document.querySelectorAll<HTMLButtonElement>(btnSelector);
+  const buttons = document.querySelectorAll<HTMLButtonElement>(btnSelector);
 
-    buttons.forEach((e) =>
-        e.addEventListener('click', () => {
-            e.setAttribute('disabled',"true");
+  buttons.forEach((e) =>
+    e.addEventListener('click', () => {
+      e.setAttribute('disabled', 'true');
 
-            const code = e.nextElementSibling?.querySelector('code');
-            const trimmed = code?.innerText.trim() // empty string denotes "failure"
-            if (trimmed) {
-                navigator.clipboard.writeText(trimmed).then(() => {
+      const code = e.nextElementSibling?.querySelector('code');
+      const trimmed = code?.innerText.trim(); // empty string denotes "failure"
+      const lang = getLanguageFromCode(code);
 
-                    e.classList.add('success');
-                    setTimeout(() => {
-                        e.classList.remove('success');
-                        e.removeAttribute('disabled');
-                    }, options.timeout);
+      if (trimmed) {
+        const textToWrite = (lang === 'bash' || lang === 'console' || lang === "shell") && trimmed.startsWith('$ ')
+          ? trimmed.substring(1).trim()
+          : trimmed;
 
-                }).catch((error) => {
-                    e.removeAttribute('disabled');
-                    console.error('error copying to clipboard', error.action);
-                })
-            }
+        navigator.clipboard.writeText(textToWrite).then(() => {
 
-        }),
-    );
+          e.classList.add('success');
+          setTimeout(() => {
+            e.classList.remove('success');
+            e.removeAttribute('disabled');
+          }, options.timeout);
+
+        }).catch((error) => {
+          e.removeAttribute('disabled');
+          console.error('error copying to clipboard', error.action);
+        });
+      }
+
+    }),
+  );
+}
+
+function getLanguageFromCode(element?: Element | null): string |undefined{
+  if (!element) return undefined;
+
+  const languageClass = Array.from(element.classList)
+    .find(className => className.startsWith('language-'));
+
+  if (!languageClass) return undefined;
+
+  return languageClass.replace('language-', '');
 }
